cdd659643e9f0df42b825245ac14577590ca3851
Devon Germano <devon.m.gemrano@gmail.com>
2018-05-13 14:11:21 -0500

    New artboard

---
+++ Assets/src/util/Credits.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour {
+
+	// Backpack Icon by Rena from the Noun Project https://thenounproject.com/search/?q=rucksack&i=1085201#
+	// Hoe Icon by Jetro Cabau Quirós from the Noun Project https://thenounproject.com/search/?q=hoe&i=52015
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	
+}
