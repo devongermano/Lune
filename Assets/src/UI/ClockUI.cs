8685800aeb83ffc8597c015c9c0563ddbe096c05
Devon Germano <devon.m.gemrano@gmail.com>
2018-05-13 16:16:58 -0500

    More minor cleanup

--- Assets/src/UI/ClockUI.cs
+++ Assets/src/UI/ClockUI.cs
@@ -3,24 +3,30 @@ using System.Collections.Generic;
 using src.Util;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class ClockUI : MonoBehaviour, ITimeListener
 {
 	private int mHours;
 	private int mMinutes;
 
-
+	private Text mText;
+	
 	private void Start()
 	{
 		TimeManager.instance.registerTimeListener(this);
+
+		mText = gameObject.GetComponent<Text>();
+	}
+
+	private void Update()
+	{
+		mText.text = mHours + ":" + mMinutes;
 	}
 
 	public void onTimeUpdate(int hours, int minutes)
 	{
 		mHours = hours;
 		mMinutes = minutes;
-		
-		Debug.Log("Hours: " + mHours);
-		Debug.Log("Minutes: " + mMinutes);
 	}
 }
