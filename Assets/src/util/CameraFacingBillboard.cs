2b8ec27c969884e5361aae6eb50c3cf6ae7d33a4
Devon Germano <devon.m.gemrano@gmail.com>
2018-05-12 19:30:12 -0500

    First Commit

---
+++ Assets/src/util/CameraFacingBillboard.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+ 
+public class CameraFacingBillboard : MonoBehaviour {
+ 
+	public Camera mCamera;
+	
+	public bool mActive;
+	public bool mAutoStart;
+	
+	private GameObject go;
+
+	
+	private void Awake(){
+		
+		if (mAutoStart) {
+			mCamera = Camera.main;
+			mActive = true;
+		}
+ 
+		//go = new GameObject();
+		
+		//go.name = transform.gameObject.name;
+		//go.transform.position = transform.position;
+		//transform.parent = go.transform;
+	}
+
+	private void Update() {
+		if (mActive) {
+			transform.LookAt(transform.position + mCamera.transform.rotation * Vector3.back, 
+				mCamera.transform.rotation * Vector3.up);
+		}
+	}
+}
---
+++ Assets/src/util/Interactable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+
+	public Camera mCamera;
+	public GameObject mInteractionVizualizer;
+
+	private Boolean updateVizualizerRotation;
+	private GameObject mInteractiveVizualizerInstance;
+	
+	
+	void Start ()
+	{
+		mInteractiveVizualizerInstance = mInteractionVizualizer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (updateVizualizerRotation)
+		{
+			mInteractiveVizualizerInstance.transform.LookAt(mInteractiveVizualizerInstance.transform.position + mCamera.transform.rotation * Vector3.forward,
+				mCamera.transform.rotation * Vector3.up);	
+		}
+			
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		mInteractiveVizualizerInstance = Instantiate(mInteractionVizualizer, transform);
+		
+		//Vector3 p = this.gameObject.transform.position;
+
+		//mInteractionVizualizer.transform.position = new Vector3(p.x, p.y + 1, p.z);
+		
+		updateVizualizerRotation = true;
+		Debug.Log("Trigger Entered!");
+		//mInteractionVizualizer.SetActive(true);
+
+		// Show the interaction interstitial
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		updateVizualizerRotation = false;
+		Destroy(mInteractiveVizualizerInstance);
+		Debug.Log("Trigger Exited!");
+		//mInteractionVizualizer.SetActive(false);
+		// Hide the interaction interstitial
+	}
+}
